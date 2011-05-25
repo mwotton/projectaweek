8bce169b470c01ad465c4dcdea2c209124633c70
Mark Wotton <mwotton@gmail.com>
2011-05-25 12:34:17 +1000

    hackers table

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.4'
 
 gem 'pg'
 gem 'sqlite3'
+gem 'rake'
 
 group :development, :test do
   gem 'rspec-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110525023129) do
+
+  create_table "hackers", :force => true do |t|
+    t.text     "twitter",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
