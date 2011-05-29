dbe694de6f3fcb76a2defb0d737838d7ef776343
Mark Wotton <mwotton@gmail.com>
2011-05-29 17:57:56 +1000

    omniauth munging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,11 @@ class SessionsController < ApplicationController
   def create
 
     auth = request.env['rack.auth']
-    Rails.logger.info(ap auth)
+    omniauth = request.env['omniauth.auth']
+    Rails.logger.info(ap :auth => auth)
+    Rails.logger.info(ap :omniauth => omniauth)
+    Rails.logger.info(ap :params => params)
+    
     self.current_hacker = Hacker.find_or_create_from_hash(auth)
     
     # Log the authorizing user in.
