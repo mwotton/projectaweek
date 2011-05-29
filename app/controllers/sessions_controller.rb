298ee18a9a43f8ae5a5a8e5f4ca3b0ee92b457d3
Mark Wotton <mwotton@gmail.com>
2011-05-29 21:09:12 +1000

    logging cleanup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,22 +2,21 @@ class SessionsController < ApplicationController
   def create
     Rails.logger.warn "in create"
     auth = request.env['omniauth.auth']
-    Rails.logger.warn(auth.inspect)
-    Rails.logger.warn(params.inspect)
+    ap :auth => auth
+    ap :params => params
     h=nil
     begin
       h = Hacker.find_or_create_from_hash(auth)
     rescue => e
-      Rails.logger.warn "Caught #{e.inspect}"
+      ap :err => e
       raise e
     end
-    
-    Rails.logger.warn("Hacker is #{h.inspect}")
+    ap :hacker => h
     self.current_hacker = h
 
     # Log the authorizing user in.
     # self.current_user = @auth.user
 
-    render :text => "Welcome, #{current_hacker.twitter}."
+    render :text => "Welcome, #{self.current_hacker.twitter}."
   end
 end
