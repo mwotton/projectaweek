42c4634d35a128de6f5d011f1a4076e3fb548114
Mark Wotton <mwotton@gmail.com>
2011-05-29 13:58:59 +1000

    order by deadline

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -88,7 +88,7 @@ class EntriesController < ApplicationController
   def scoreboard
     #    topHackers = params[:to_show] || 10
     @hackers = Hacker.includes(:entries).order("entries_count desc")
-    @rounds = Round.where("deadline < ?", Time.now)
+    @rounds = Round.where("deadline < ?", Time.now).order(:deadline)
     
     @entries = {}
     @hackers.each do |h|
