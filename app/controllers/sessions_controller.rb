dda4ef151329f83ab88f03e1a9ea77677872ca48
Mark Wotton <mwotton@gmail.com>
2011-05-29 17:59:16 +1000

    more logging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def create
-
+    Rails.logger.warn "in create"
     auth = request.env['rack.auth']
     omniauth = request.env['omniauth.auth']
-    Rails.logger.info(ap :auth => auth)
-    Rails.logger.info(ap :omniauth => omniauth)
-    Rails.logger.info(ap :params => params)
+    Rails.logger.warn(ap :auth => auth)
+    Rails.logger.warn(ap :omniauth => omniauth)
+    Rails.logger.warn(ap :params => params)
     
     self.current_hacker = Hacker.find_or_create_from_hash(auth)
     
