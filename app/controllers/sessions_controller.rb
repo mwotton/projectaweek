dd85c7f77c71efd3b9379cd8d09e627aec3482ac
Mark Wotton <mwotton@gmail.com>
2011-05-29 17:34:44 +1000

    logging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
   def create
+
     auth = request.env['rack.auth']
-    unless @hacker = Hacker.find_from_hash(auth)
-      @auth = Hacker.create_from_hash(auth, current_user)
-    end
+    ap auth
+    self.current_hacker = Hacker.find_or_create_from_hash(auth)
+    
     # Log the authorizing user in.
     self.current_user = @auth.user
 
