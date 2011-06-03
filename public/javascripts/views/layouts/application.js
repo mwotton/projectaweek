184a81eadb7d48850dd60a71134791af46127f55
scheinercc <albert@scheiner.cc>
2011-06-04 02:47:52 +1000

    create script for random background color on page load

--- public/javascripts/views/layouts/application.js
+++ public/javascripts/views/layouts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+  var colors = ["#00ff99", "#444", "#aaa", "#9cff00", "#005980", "#00b4ff"];                
+  var rand = Math.floor(Math.random()*colors.length);           
+  $('body').css("background-color", colors[rand]);
+});
